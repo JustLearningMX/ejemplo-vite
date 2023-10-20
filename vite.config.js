29f9ec053d8d507058644bbb52f10fc3cd010196
Hiram Chavez <virtual.liga@gmail.com>
2023-10-20 00:52:17 -0600

    commit 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ejemplo-vite/',
+  build: {
+    outDir: 'docs', // Configura la carpeta de salida como 'docs'
+  },
   plugins: [react()],
 })
